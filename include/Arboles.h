7044133483adb00dd68090a03b2e3feb718e842a
Luis Montoya <devlumniom@gmail.com>
2023-05-27 17:51:03 -0600

    Primer commit

---
+++ include/Arboles.h
@@ -0,0 +1,14 @@
+//
+// Created by LAMM_ on 27/05/2023.
+//
+
+#ifndef PRACTICA12_ARBOLES_H
+#define PRACTICA12_ARBOLES_H
+
+typedef struct NodoArbol{
+
+} NodoArbol;
+
+
+
+#endif //PRACTICA12_ARBOLES_H
